2479ef4a208bfd8a146e97dbc3918bed236aec65
chiaraamerio <100305899+chiaraamerio@users.noreply.github.com>
2022-05-17 21:55:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,7 +23,10 @@ int main() {
 
     do {
         x=(a+b)/2;
-        if(f(a)*f(x)>0){
+        if(f(x)==0.0){
+            break;
+        }
+        else if(f(a)*f(x)>0){
             a=x;
         }else{
             b=x;
@@ -31,8 +34,8 @@ int main() {
         err=abs((b-a)/2);
     }while(err > 1e-6);
 
-    cout<<x<<endl;
-    cout<<f(x)<<endl;
-
+    cout<<int(x*10000/10000.0)<<endl;
+    
+  
    return 0;
 }
